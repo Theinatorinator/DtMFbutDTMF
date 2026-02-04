d3e8b5706a4839af6af83e94e8f85ef4f378d162
Theinatorinator <Logan.mamanakis@gmail.com>
2026-02-03 23:25:16 -0700

    Add ruff configuration file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def main():
-    print("Hello from dtmfbutdtmf!")
+"""Main entry point for the DTMF application."""
+
+
+def main() -> None:
+    """Entry point for the DTMF application."""
 
 
 if __name__ == "__main__":
